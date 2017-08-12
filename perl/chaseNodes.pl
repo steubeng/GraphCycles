77abcf113896c13a026cab55ca7257414a227441
Gregg K. Steuben <steuben@ge.com>
2017-08-11 23:17:00 -0400

    cleaned perl script

--- perl/chaseNodes.pl
+++ perl/chaseNodes.pl
@@ -1,9 +1,12 @@
+$csvDelimeter = ";";
+
+# read in data file
 open(INPUT, $ARGV[0]) || die "Cannot open input file";
 @data = <INPUT>;
 close(INPUT);
 
 for ($i=2 ; $i < @data ; $i++) {
-    @line = split(/\s+/, $data[$i]);
+    @line = split(/$csvDelimeter/, $data[$i]);
     if ($line[1] eq "1") {
 	@node = split("_", $line[0]);
 	$node[0] =~ s/x(.*)/$1/;
@@ -11,39 +14,42 @@ for ($i=2 ; $i < @data ; $i++) {
     }
 }
 
+# build a keySet list
 foreach $key (keys(%edge)) {
     push(@keySet, $key);
 }
 
+# initialize sum variables
 $sumNodes = 0;
 $sumRealNodes = 0;
 $sumDummies = 0;
 
+# iterate through all the keys and for each key find the cycle that it is a part of keeping track of cycle size in terms of nodes
 for ($i=0 ; $i < @keySet ; $i++) {
     $startNode = $keySet[$i];
-    while ($visited{$startNode} eq "1") {
+    while ($visited{$startNode} eq "1") { # a node may already have bene visited, if so skip it until we find an unvisited one
 	$i++;
 	$startNode = $keySet[$i];
     }
-    if ($startNode eq "") {
+    if ($startNode eq "") { # might wind up with an empty startNode because we've skipped to the end of the list of keys
 	last;
     }
     print "startNode: '" . $startNode . "'\n";
     $nodes = 0;
     $dummies = 0;
-    $nextNode = $startNode;
+    $nextNode = $startNode; # initialize nextNode
     while (true) {
 	$nodes++;
 	print "\t";
 	print $edge{$nextNode};
-	if ($edge{$nextNode} =~ /^dummy/) {
+	if ($edge{$nextNode} =~ /^dummy/) { # found a dummy
 	    $dummies++;
 	    print " (" . $dummies . ")";
 	}
 	print "\n";
-	$visited{$nextNode} = "1";
-	$nextNode = $edge{$nextNode};
-	if ($nextNode eq $startNode) {
+	$visited{$nextNode} = "1"; # mark this node as 'visited'
+	$nextNode = $edge{$nextNode}; # advance to the next node in the cycle
+	if ($nextNode eq $startNode) { # we've come to the end of the cycle because we're looking at the startNode again.
 	    $sumNodes += $nodes;
 	    $sumRealNodes += ($nodes - $dummies);
 	    $sumDummies += $dummies;
